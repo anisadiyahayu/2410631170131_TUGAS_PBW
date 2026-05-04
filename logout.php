4a50d09071b4cd068000eaeb24abee0bd0fab6f3
anisadiyahayu <anisadiyahayu24@gmail.com>
2026-05-04 22:03:38 +0700

    hapus file root yang salah

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-session_unset();
-session_destroy();
-
-header("Location: login.php?message=" . urlencode("Anda berhasil logout."));
-exit;
-?>
--- session_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['login_Un51k4']) || $_SESSION['login_Un51k4'] !== true) {
-    header("Location: login.php?message=" . urlencode("Silakan login terlebih dahulu."));
-    exit;
-}
-?>
